11adf1703df789d64c444fcd7687a13ae050321a
Marlon O'Neil <hello@marlononeil.com>
2017-08-29 22:59:31 -0400

    completed stretch 'Make sure the user can't pick up more than three cards.'

--- exercise.rb
+++ exercise.rb
@@ -4,13 +4,19 @@ def select_cards(possible_cards, hand)
   possible_cards.each do |current_card|
     print "Do you want to pick up #{current_card}?"
     answer = gets.chomp
+    # binding.pry
     if answer.downcase == 'y'
       hand << current_card
+      if hand.length == 3
+        puts
+        puts "You have selected #{hand.length} cards, you cannot select any more"
+        puts
+        return hand
+      end
     end
   end
 
   return hand
-
 end
 
 available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
